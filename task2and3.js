f5213a697f8e40c5e48ecafa8e00399e8e4aefcc
antroot <48415665+antroot@users.noreply.github.com>
2019-03-11 01:26:32 +0300

    Update and rename main.js to task2and3.js

--- task2and3.js
+++ task2and3.js
@@ -1,21 +1,6 @@
 (function () {
 
     'use strict'
-    
-    // Напишите функцию, которая получает в качестве аргумента массив объектов координат и превращает его в строку,
-    // подходящую для атрибута points  svg-тега <polygon>.
-    // Пример массива: [{x:12, y:13}, {x:56, y: 45}, ...]
-
-    let star = [{x:70, y:5}, {x:90, y: 41}, {x:136, y: 48}, {x:103, y:80}, {x:111, y:126},
-                  {x:70, y:105}, {x:29, y:126}, {x:36, y:80}, {x:5, y:48}, {x:48, y:41}];
-
-    let points = star.map((e) => (`${e.x},${e.y}`)).join(" ");
-
-    let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    let polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-    document.body.appendChild(svg);
-    svg.appendChild(polygon);
-    polygon.setAttribute("points", points);
 
     // Напишите функцию, которая будет получать в качестве аргумента массив чисел,
     // и будет возвращать массив только тех чисел, которые не повторяются.
@@ -59,4 +44,3 @@
     console.log(splitString(tempestString, br));
 
 })();
-
